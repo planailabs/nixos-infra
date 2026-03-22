25545d09c3346f097e0e3914d0a194916c97e251
Maciej Krüger <mkg20001@gmail.com>
2026-03-22 10:17:11 +0100

    feat: use dns for deploy

--- chronos.sh
+++ chronos.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-NIX_SSHOPTS="-p 22222" nixos-rebuild --flake .#chronos --use-substitutes --target-host root@2a01:4f8:242:1ae1:1:a:0:c --impure switch
+NIX_SSHOPTS="-p 22222" nixos-rebuild --flake .#chronos --use-substitutes --target-host root@chronos.plan.ai --impure switch
--- logos.sh
+++ logos.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-nixos-rebuild --flake .#logos --use-substitutes --target-host root@2a01:4f8:242:1ae1:1:a:0:e --impure switch
+nixos-rebuild --flake .#logos --use-substitutes --target-host root@logos.plan.ai --impure switch
