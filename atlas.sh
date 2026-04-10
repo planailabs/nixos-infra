f35f1379466cf50bfbcdb33db1e9e86ad6770a4d
Admin <admin@plan.ai>
2026-04-10 12:35:57 +0200

    atlas: use boot

--- atlas.sh
+++ atlas.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-nixos-rebuild --flake .#atlas --use-substitutes --target-host root@atlas.plan.ai --impure switch
+nixos-rebuild --flake .#atlas --use-substitutes --target-host root@atlas.plan.ai --impure boot
