0fa8225ffd6732a19fefeec080765686a01fc26f
Admin <admin@plan.ai>
2026-04-16 17:59:57 +0200

    feat: yggdrasil ip pi

--- modules/yggdrasil-ips.nix
+++ modules/yggdrasil-ips.nix
@@ -3,4 +3,5 @@
   chronos = "201:874a:1ac7:bb35:63e7:fbd8:6022:a541";
   deploy = "202:f39e:d653:c95a:863e:23f:120:c379";
   logos = "203:fa0d:fb88:3210:1716:321e:de5f:ed59";
+  pi = "200:7ef6:32c6:540a:d4:3e52:6e1c:fdd9";
 }
